7b909239bc540c985d6ee771c1b675d91977580c
Sam <74021826+x39OME@users.noreply.github.com>
2022-12-26 21:11:21 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// Toggle Menu
+function menuToggle(){
+  const toggleMenu = document.querySelector('.toggle');
+  const section = document.querySelector('section');
+  toggleMenu.classList.toggle('active')
+  section.classList.toggle('active')
+}
