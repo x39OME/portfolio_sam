05b7eaf8bddad49387b903e2eeb190296c8cda43
Sam <74021826+x39OME@users.noreply.github.com>
2023-02-10 21:54:43 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,3 +5,23 @@ function menuToggle(){
   toggleMenu.classList.toggle('active')
   section.classList.toggle('active')
 }
+
+
+
+// Time & Date (Footer)
+setInterval(function(){
+  document.getElementById('date').innerHTML = Date();
+  },1000);
+  
+  // Scroll To Top
+  let scrollTop = document.querySelector('.scrollTop');
+  
+  window.onscroll = function (){
+    this.scrollY >= 1000 ? scrollTop.classList.add('show') : scrollTop.classList.remove('show');
+  };
+  scrollTop.onclick = function(){
+    window.scrollTo({
+      top: 0,
+      behavior:'smooth'
+    });
+  };
